378760532b45d5e447d846847dcf0d6eb3083be0
Rohmat Subekti <rohmatsb@gmail.com>
2024-07-24 22:44:47 -0700

    Update linux-pingbughost.sh

--- linux-pingbughost.sh
+++ linux-pingbughost.sh
@@ -1,5 +1,5 @@
 while true; do
-        ping -c 2 img.email3.vidio.com
-        ping -c 2 quiz.vidio.com
-        sleep 30
+  ping -c 2 img.email3.vidio.com
+  ping -c 2 quiz.vidio.com
+  sleep 30
 done
